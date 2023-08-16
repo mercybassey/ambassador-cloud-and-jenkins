bfe1faf96f7eb29f2b89b7e9a8e1d31e8e5b3e16
mercybassey <udohmercy911@gmail.com>
2023-08-16 19:39:30 +0100

    second commit

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const PORT = 3000;
 
 // Root route
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome to the Home Page!</h1>');
+    res.send('<h1>Welcome to the new Home Page!</h1>');
 });
 
 // About route
 app.get('/about', (req, res) => {
-    res.send('<h1>This is the About Us Page.</h1>');
+    res.send('<h1>This is the new About Us Page.</h1>');
 });
 
 // Contact route
 app.get('/contact', (req, res) => {
-    res.send('<h1>This is the Contact Us Page.</h1>');
+    res.send('<h1>This is the new Contact Us Page.</h1>');
 });
 
 // Start the server
